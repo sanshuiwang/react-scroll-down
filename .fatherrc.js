eafb31fc07d0ac3698498e3e74ef377ab0d8a08e
wangyang <768188667@qq.com>
2019-11-04 20:45:06 +0800

    rollup to support hooks

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,15 +1,23 @@
+import react from 'react'
+import commonjs from 'rollup-plugin-commonjs'
+
 export default {
   esm: {
     type: 'rollup'
   },
   cjs: 'rollup',
   umd: {
-    name: 'FatherReactUi',
+    name: 'ReactScrollDown',
     globals: {
       react: 'React'
     }
   },
   cssModules: {
     generateScopedName: '[name]__[local]___[hash:base64:5]'
-  }
+  },
+  extraRollupPlugins: [
+    commonjs({
+      namedExports: { react: Object.keys(react) }
+    })
+  ]
 }
